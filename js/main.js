db41c23cbc74e297b3ba1dfe3e6ab33dcf46b6a0
Andrea Torelli <gioran326@protonmail.com>
2023-03-08 23:28:38 +0100

    migliorie

--- js/main.js
+++ js/main.js
@@ -1,22 +1,32 @@
-const elencoNumeriDom = document.getElementById("elencoNumeriDom");
+const elencoNumeriDom = document.getElementById("elencoNumeri");
+let startDom = document.getElementById("start");
 let numeriDaStampare = 1;
-let rangeMax = 100;
+let tempo = 2500;
+let rangeMax = 10;
 let elencoNumeriDaIndovinare = [];
 let elencoNumeriIndovinati = [];
-var continuare = false;
+let punteggio = [];
+let continuare = false;
 
-avviaProgramma();
+startDom.addEventListener("click",avviaProgramma)
 
 function avviaProgramma (){
+   let schermataInizialeDom = document.getElementById("schermataIniziale");
+   schermataInizialeDom.innerHTML = "";
     for (i = 0; i < numeriDaStampare; i++) {
-        elencoNumeriDaIndovinare.push(generaNumeroRandom(1, rangeMax));
-     }
+      elencoNumeriDaIndovinare.push(generateUniqueRandomNumber(elencoNumeriDaIndovinare, 1, rangeMax));
+      let numero = `<div>${elencoNumeriDaIndovinare[i]}</div>`;
+      elencoNumeriDom.innerHTML += numero;
+      elencoNumeriDom.classList.remove("d-none");
+
+   }
   
-     document.getElementById("elencoNumeriDom").innerHTML = elencoNumeriDaIndovinare;
-     setTimeout(verifica,numeriDaStampare * 3000);
+     setTimeout(verifica,tempo * numeriDaStampare);
 }
 
 function verifica() {
+   let risultatoDom = document.getElementById("risultato");
+
    elencoNumeriDom.classList.add("d-none");
    for (x = 0; x < numeriDaStampare; x++) {
      let numeroPrompt = parseInt(prompt("Quali numeri ricordi?"));
@@ -25,7 +35,8 @@ function verifica() {
       }
    }
    
-   document.getElementById("risultato").innerHTML = `Hai indovinato ${elencoNumeriIndovinati.length} numeri(${elencoNumeriIndovinati})`;
+   risultatoDom.classList.remove("d-none");
+   risultatoDom.innerHTML = `<span>Hai indovinato ${elencoNumeriIndovinati.length} numeri(${elencoNumeriIndovinati})</span>`;
    if (elencoNumeriIndovinati.length >= elencoNumeriDaIndovinare.length / 2) {
       continuare = confirm("Vuoi continuare?");
       if (continuare == true) {
@@ -33,14 +44,35 @@ function verifica() {
          elencoNumeriDaIndovinare = [];
          elencoNumeriIndovinati = [];
          elencoNumeriDom.classList.remove("d-none");
-         document.getElementById("risultato").innerHTML = ``;
+         elencoNumeriDom.innerHTML = "";
+         risultatoDom.innerHTML = ``;
          avviaProgramma();
       }
    } 
 } 
 
+function generateUniqueRandomNumber(array, min, max) {
 
-function generaNumeroRandom(min, max) {
-   const numeroRandom = Math.floor( Math.random() * (max - min + 1) ) + min;
-   return numeroRandom;
+   let isValidNumber = false;
+   let randomNumber;
+
+   while (!isValidNumber) {
+      randomNumber = Math.floor( Math.random() * (max - min + 1) ) + min;
+      if (!array.includes(randomNumber)) {
+         isValidNumber = true;
+      }
+   }
+   return randomNumber;
 }
+
+/*
+
+-selettore che aumenti il range dei numeri e cali il tempo
+-tasto per non aspettare la fine del tempo
+-sostituire la prompt "vuoi continuare?" con div+button
+-messaggio diverso se perdi
+-se clicchi su "annulla" nella prompt, game over/termina il gioco
+-contatore punteggio?!
+-migliorare grafica (soprattutto smartphone)
+
+*/
