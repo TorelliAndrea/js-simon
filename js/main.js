996a2b28a4a7f4e352f7a05ffd61084d9cd9cf78
Andrea Torelli <gioran326@protonmail.com>
2023-08-29 16:57:25 +0200

    Fixato il bug della difficoltà

--- js/main.js
+++ js/main.js
@@ -1,55 +1,85 @@
 const elencoNumeriDom = document.getElementById("elencoNumeri");
-let startDom = document.getElementById("start");
+let select = document.getElementById("difficolta");
 let numeriDaStampare = 1;
-let tempo = 2500;
-let rangeMax = 10;
 let elencoNumeriDaIndovinare = [];
-let elencoNumeriIndovinati = [];
-let punteggio = [];
-let continuare = false;
+// let punteggio = [];
 
-startDom.addEventListener("click",avviaProgramma)
+select.addEventListener("change",avviaProgramma)
 
 function avviaProgramma (){
    let schermataInizialeDom = document.getElementById("schermataIniziale");
    schermataInizialeDom.innerHTML = "";
-    for (i = 0; i < numeriDaStampare; i++) {
-      elencoNumeriDaIndovinare.push(generateUniqueRandomNumber(elencoNumeriDaIndovinare, 1, rangeMax));
+   let time;
+   let rangeMax;
+
+   if (select.value == "facile"){
+      time = 5000;
+      rangeMax = 9;
+   } else if (select.value == "medio"){
+      time = 3000;
+      rangeMax = 50; 
+   } else if (select.value == "difficile"){
+      time = 2000;
+      rangeMax = 100; 
+   } else if (select.value == "pro"){
+      time = 1000;
+      rangeMax = 1000; 
+   }
+   
+   for (i = 0; i < numeriDaStampare; i++) {
+      elencoNumeriDaIndovinare.push(generateUniqueRandomNumber(elencoNumeriDaIndovinare, 0, rangeMax));
       let numero = `<div>${elencoNumeriDaIndovinare[i]}</div>`;
       elencoNumeriDom.innerHTML += numero;
       elencoNumeriDom.classList.remove("d-none");
-
    }
-  
-     setTimeout(verifica,tempo * numeriDaStampare);
+
+   setTimeout(verifica,time * numeriDaStampare);
 }
 
 function verifica() {
+   let elencoNumeriIndovinati = [];
    let risultatoDom = document.getElementById("risultato");
+   let continuare = false;
 
    elencoNumeriDom.classList.add("d-none");
    for (x = 0; x < numeriDaStampare; x++) {
-     let numeroPrompt = parseInt(prompt("Quali numeri ricordi?"));
+      let numeroPrompt = parseInt(prompt("Quali numeri ricordi?"));
       if (elencoNumeriDaIndovinare.includes(numeroPrompt)){
          elencoNumeriIndovinati.push(numeroPrompt);
+      } else if (numeroPrompt === NaN){                          //non entra mai qua... in caso di "Annulla" nel prompt, vorrei terminare subito il gioco
+         x=999; 
+         GameOver ();
       }
    }
    
    risultatoDom.classList.remove("d-none");
-   risultatoDom.innerHTML = `<span>Hai indovinato ${elencoNumeriIndovinati.length} numeri(${elencoNumeriIndovinati})</span>`;
    if (elencoNumeriIndovinati.length >= elencoNumeriDaIndovinare.length / 2) {
-      continuare = confirm("Vuoi continuare?");
-      if (continuare == true) {
-         numeriDaStampare++;
-         elencoNumeriDaIndovinare = [];
-         elencoNumeriIndovinati = [];
-         elencoNumeriDom.classList.remove("d-none");
-         elencoNumeriDom.innerHTML = "";
-         risultatoDom.innerHTML = ``;
-         avviaProgramma();
-      }
+      risultatoDom.innerHTML = `<span>Complimenti!!! Hai indovinato ${elencoNumeriIndovinati.length} numeri (${elencoNumeriIndovinati})</span>`;
+      setTimeout(() => {
+         continuare = confirm("Vuoi continuare?");
+         if (continuare == true) {
+            numeriDaStampare++;
+            elencoNumeriDaIndovinare = [];
+            elencoNumeriIndovinati = [];
+            elencoNumeriDom.classList.remove("d-none");
+            elencoNumeriDom.innerHTML = "";
+            risultatoDom.innerHTML = ``;
+            avviaProgramma();
+         }  else {
+            GameOver();
+         }
+      }, 2000);
    } 
-} 
+
+   function GameOver() {
+
+   risultatoDom.innerHTML = `<span>Hai indovinato ${elencoNumeriIndovinati.length} numeri (${elencoNumeriIndovinati})</span>
+                                 <button id="Rigioca" class="btn btn-lg btn-outline-danger">Rigioca</button>`;
+      let rigioca = document.getElementById("Rigioca");
+      select = null;
+      rigioca.addEventListener('click', () => location.reload());
+   }
+}
 
 function generateUniqueRandomNumber(array, min, max) {
 
@@ -65,14 +95,11 @@ function generateUniqueRandomNumber(array, min, max) {
    return randomNumber;
 }
 
-/*
 
--selettore che aumenti il range dei numeri e cali il tempo
--tasto per non aspettare la fine del tempo
--sostituire la prompt "vuoi continuare?" con div+button
--messaggio diverso se perdi
--se clicchi su "annulla" nella prompt, game over/termina il gioco
--contatore punteggio?!
--migliorare grafica (soprattutto smartphone)
+
+/*
+   -tasto per non aspettare la fine del tempo
+   -contatore punteggio?!
+   -migliorare grafica (soprattutto smartphone)
 
 */
